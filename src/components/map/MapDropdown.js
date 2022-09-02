4d0a69dd3ea2e79c4114f3c3130976857e5c2918
MageForth <mageforth@gmail.com>
2022-09-02 16:07:04 +0430

    :zap: dropdown and selecting on search functionality added

--- src/components/map/MapDropdown.js
+++ src/components/map/MapDropdown.js
@@ -1,22 +1,84 @@
-import { useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import 'styles/mapDropdown.scss'
+import { ReactComponent as SearchIcon } from 'assets/icons/icons8-search-30.svg'
+import UserContext from 'contexts/UserContext'
 
 const MapDropDown = () => {
+  const { userData } = useContext(UserContext)
+  const [search, setSearch] = useState('')
+  const [dropdownItems, setDropdownItems] = useState([])
+  const [selected, setSelected] = useState(null)
+
+  const selectHandler = (e) => {
+    setSelected({
+      id: e.target.getAttribute('data-id'),
+      name: e.target.innerText,
+    })
+
+    setSearch(e.target.innerText)
+
+    setDropdownItems([])
+  }
+
   useEffect(() => {
-    const fetcher = async () => {
-      const res = await fetch(
-        `https://exam.pishgamanasia.com/webapi/Request/GetVehicleUsers?SearchTerm=Industrial&UserToken=7f5a46e2`
-      )
-      console.log(res)
-      const data = await res.json()
-      console.log(data)
+    const abortController = new window.AbortController()
+    const signal = abortController.signal
+
+    selected && setSelected(null)
+
+    if (search.length > 2 && !selected) {
+      const fetcher = async () => {
+        try {
+          const res = await fetch(
+            `https://exam.pishgamanasia.com/webapi/Request/GetVehicleUsers?SearchTerm=${search}&UserToken=${userData}`,
+            { signal }
+          )
+          const data = await res.json()
+          setDropdownItems([...data.data])
+        } catch (error) {
+          // Abort meesage will show in here
+        }
+      }
+      fetcher()
     }
-    fetcher()
-  }, [])
+
+    return () => {
+      if (signal && abortController.abort) {
+        abortController.abort()
+      }
+    }
+  }, [search])
 
   return (
     <div className='mapDropdown'>
-      <input type='text' />
+      <div className='mapDropdown__locations'>
+        <span>afds</span>
+        <span>sfd</span>
+      </div>
+
+      <div className='mapDropdown__dropdown__container'>
+        <ul className='mapDropdown__dropdown'>
+          {dropdownItems.map((item) => (
+            <li data-id={item.id} key={item.id} onClick={selectHandler}>
+              {item.name}
+            </li>
+          ))}
+        </ul>
+        <div className='mapDropdown__input'>
+          <input
+            type='text'
+            onChange={(e) => {
+              setSearch(e.target.value)
+              if (e.target.value.trim() === '') setDropdownItems([])
+            }}
+            value={search}
+            placeholder='نوع ماشین آلات'
+          />
+          <SearchIcon />
+        </div>
+      </div>
+
+      <button className='mapDropdown__submit'>ثبت درخواست</button>
     </div>
   )
 }
